f04398c15856dfbd0493acd7ec0bb8552203a041
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 14:21:34 +0200

    Update nationalityDBController.js

--- db/controllers/nationalityDBController.js
+++ db/controllers/nationalityDBController.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const Nationalities = require("../models/nationalityModel");
+const Nationality = require("../models/nationalityModel");
 
 const NON_SELECTED_FIELDS = "-__v";
 
 const findAllActive = function () {
-  return Nationalities.find({ active: true }, NON_SELECTED_FIELDS).sort({ description: 1 }).lean().exec();
+  return Nationality.find({ active: true }, NON_SELECTED_FIELDS).sort({ description: 1 }).lean().exec();
 };
 
 module.exports = { findAllActive };
