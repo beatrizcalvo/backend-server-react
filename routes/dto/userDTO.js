675cdec42a56f5fc31072b23d3d827a28e3881e6
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 23:16:59 +0200

    Update userDTO.js

--- routes/dto/userDTO.js
+++ routes/dto/userDTO.js
@@ -1,6 +1,34 @@
 const userResponseDTO = (userDB, profileDB) => {
   return {
-    userId: userDB._id
+    userId: userDB._id,
+    typeCode: profileDB.role.code,
+    typeDescription: profileDB.role.description,
+    person: {
+      personName: {
+        firstName: userDB.profile.firstName,
+        lastName: userDB.profile.lastName,
+        secondLastName: userDB.profile.secondLastName,
+        fullName: userDB.profile.firstName + " " + userDB.profile.lastName + 
+          (!!userDB.profile.secondLastName ? (" " + userDB.profile.secondLastName) : ""), 
+      },
+      gender: userDB.profile.gender,
+      birthDate: userDB.profile.birthDate,
+      ...(!!userDB.profile.nationality && {
+        firstNationality: {
+          code: userDB.profile.nationality.code,
+          description: userDB.profile.nationality.description
+        }
+      })
+    },   
+    contactPoint: {
+      electronicAddress: {
+        emailAddress: userDB.email
+      }
+    },
+    audit: {
+      creationDate: userDB.createdAt,
+      modificationDate: userDB.updatedAt
+    }
   };
 };
 
