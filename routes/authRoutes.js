726ff024af9f7f56ec9595f44d63ab2b49b90e97
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 21:35:37 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -98,7 +98,25 @@ router.post("/refresh", validateRequest(refreshSchema), (req, res, next) => {
   try {
     // Verify the token and check if the token exists. Any error will return code 401
     const decodedToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET_KEY);
-    
+
+    // Check if refresh token exists in database
+    userTokenDBController.findByToken(refreshToken)
+      .then(userToken => {
+        // Check if userId in database is equal to sub in refresh token
+        if (userToken.userId.toString() !== decodedToken.sub) {
+          return next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
+        }
+
+        // Create new access token
+        const newAccessToken = createToken(decodedToken.sub, process.env.ACCESS_TOKEN_SECRET_KEY, ACCESS_TOKEN_EXPIRES_IN);
+        const responseBody = createResponseTokens(newAccessToken, refreshToken);
+        console.log('POST /auth/refresh ## Request Body: {"refresh_token": "' + refreshToken + '"} || Response Status: 201 ## Response Body: ' + 
+                    JSON.stringify(responseBody));
+        res.status(201).send(responseBody);
+      })
+      .catch(() => {
+        next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
+      });
   } catch (error) {
     next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
   }
