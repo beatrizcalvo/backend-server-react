85640b1ab8781832139957d753c860bca4020613
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 14:08:34 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,30 @@ require("dotenv").config();
 
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 const errorMessages = require("../constants/errorConstants");
 const validateRequest = require("../middlewares/validateRequest");
 const { loginSchema, registerSchema, refreshSchema } = require("../validators/authValidator");
 
 router.post("/register", validateRequest(registerSchema), (req, res, next) => {
-  console.log(req.headers);
-  res.status(201).send({});
+  const firstName = req.body.firstName.trim();
+  const lastName = req.body.lastName.trim();
+  const email = req.body.email.trim().toLowerCase();
+  const password = req.body.password;
+
+  // Hash the password
+  bcrypt.hash(password, 10)
+    .then(hashedPassword => {
+      const capitalizedFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+      const capitalizedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+      // Save the new user
+      res.status(201).send({});
+    })
+    .catch(error => {
+      next(createHttpError(500, error));
+    });
 });
 
 module.exports = router;
