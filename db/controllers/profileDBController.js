a9e245440c57c917b4de12d49cf177183c129b12
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 22:38:07 +0200

    Create profileDBController.js

---
+++ db/controllers/profileDBController.js
@@ -0,0 +1,9 @@
+const Profile = require("../models/profileModel");
+
+const NON_SELECTED_FIELDS = "-__v";
+
+const findByIdPopulated = function (id) {
+  
+};
+
+module.exports = { findByIdPopulated }
