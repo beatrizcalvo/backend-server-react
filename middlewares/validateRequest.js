4207a95976dbb88801e03bdb7a2dc22badab49c5
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 13:45:27 +0200

    Create validateRequest.js

---
+++ middlewares/validateRequest.js
@@ -0,0 +1,59 @@
+const createHttpError = require('http-errors');
+const Joi = require('joi');
+
+const errorMessages = require("../constants/errorConstants");
+
+const validateRequest = (schema) => function (req, res, next) { 
+  try {
+    // Delete withespaces in all input body fields
+    req.body = trimObjValues(req.body);
+
+    // Validate all input body fields
+    const result = schema.validate(req.body, { abortEarly: false});
+    if (result.error) return next(createHttpError(400, createValidationErrors(result.error)));
+    next();    
+  } catch (error) {
+    next(createHttpError(500, error));
+  }
+};
+
+const trimObjValues = function (obj) {
+  if ((typeof obj) === "string") return obj.trim();
+  if ((typeof obj) === "object") 
+    return Object.fromEntries(Object.keys(obj).map(key => [key, trimObjValues(obj[key])]));
+  return obj;
+};
+
+const createValidationErrors = function (error) {
+  const listErrors = [];
+  error.details.map(err => {
+    const field = err.path.join(".");
+    switch (err.type) {
+      case "any.empty":
+      case "any.required":
+      case "string.empty":
+        listErrors.push(errorMessages.AUTH_API_F_0001(field));
+        break;
+      case "any.only":
+        listErrors.push(errorMessages.AUTH_API_F_0010(field, err.context.valids.join(", ")));
+        break;
+      case "date.base":
+      case "date.format": 
+      case "string.base":
+      case "string.email":
+        listErrors.push(errorMessages.AUTH_API_F_0002(field));
+        break;
+      case "string.min":
+        listErrors.push(errorMessages.AUTH_API_F_0003(field, err.context.limit));
+        break;
+      case "object.unknown":
+        listErrors.push(errorMessages.AUTH_API_F_0004(field));
+        break;
+      default: 
+        break;
+    }
+  });
+  return JSON.stringify(listErrors);
+};
+
+module.exports = validateRequest;
