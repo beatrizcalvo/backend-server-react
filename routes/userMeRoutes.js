a6ead76be834ef831058e1c709e085bc47479b15
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 22:42:05 +0200

    Update userMeRoutes.js

--- routes/userMeRoutes.js
+++ routes/userMeRoutes.js
@@ -5,12 +5,16 @@ const router = express.Router();
 const errorMessages = require("../constants/errorConstants");
 
 const userDBController = require("../db/controllers/userDBController");
+const profileDBController = require("../db/controllers/profileDBController");
 const { userResponseDTO } = require("./dto/userDTO");
 
 router.get("/me", (req, res, next) => {
   userDBController.findById(req.currentUserId)
     .then(user => {
-      
+      profileDBController.findByIdPopulated(user.profileId)
+        .then(profile => {
+          
+        });
     })
     .catch(error => {
       next(createHttpError(404, JSON.stringify([errorMessages.AUTH_API_F_0008()])));
