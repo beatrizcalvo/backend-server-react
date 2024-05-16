8a29357e0368aabe5766bf7d3287abe6b04fb804
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 17:27:38 +0200

    Create profileModel.js

---
+++ db/models/profileModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please provide a first name!"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please provide a last name!"],
+    },
+    secondLastName: { type: String },
+    gender: {
+      type: String,
+      enum: ["Female", "Male"]
+    }, 
+    birthDate: {
+      type: Date,
+      max: new Date() 
+    },
+    nationality: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Nationalities"
+    },
+    role: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Roles",
+      required: [true, "Please provide a role!"],
+    }
+  },
+  { timestamps: true },
+);
+
+// profiles is collection name
+const db = mongoose.connection.useDb("reactApp");
+const profilesModel = db.model('profiles', ProfileSchema);
+
+module.exports =  profilesModel;
