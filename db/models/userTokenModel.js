51721db61c02ee5417cde46e8a3e8305a9157ad2
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 18:32:27 +0200

    Create userTokenModel.js

---
+++ db/models/userTokenModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UserTokenSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Please provide a user id!"]
+    },
+    token: {
+      type: String,
+      required: [true, "Please provide a token!"]
+    }
+  },
+  { timestamps: true }
+);
+
+// userstokens is collection name
+const authdb = mongoose.connection.useDb("authDB");
+const usersTokensModel = authdb.model('userstokens', UserTokenSchema);
+
+module.exports =  { usersTokensModel };
