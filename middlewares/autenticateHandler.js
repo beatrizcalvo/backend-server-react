11265969ca6fbd7ddee382f231671e7ef7e9aaa8
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 21:44:26 +0200

    Update autenticateHandler.js

--- middlewares/autenticateHandler.js
+++ middlewares/autenticateHandler.js
@@ -19,7 +19,20 @@ const autenticateHandler = (req, res, next) => {
   try {
     // Verify the token and check if the user exists. Any error will return code 401
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
-    
+
+    // Check if a user with this id exists in the database
+    userDBController.findById(decodedToken.sub)
+      .then(user => {
+        // Check if user is active
+        if (!user.active) return next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0009()])));
+        
+        // Save current user id in request
+        req.currentUserId = decodedToken.sub;
+        next();
+      })
+      .catch(() => {
+        next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
+      });
   } catch (error) {
     next(createHttpError(401, JSON.stringify([errorMessages.AUTH_API_F_0007()])));
   }
