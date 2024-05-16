102715fb380d4ee29b1a56d11e90dd2f7d793a7d
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 23:58:47 +0200

    Update profileModel.js

--- db/models/profileModel.js
+++ db/models/profileModel.js
@@ -25,7 +25,7 @@ const ProfileSchema = new mongoose.Schema(
     },
     role: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "roles",
+      ref: "Role",
       required: [true, "Please provide a role!"],
     }
   },
