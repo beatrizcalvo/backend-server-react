ffbef6d6cacf4edbb5d1ba2af91ad7ff19fcfbc0
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 21:58:45 +0200

    Update userMeRoutes.js

--- routes/userMeRoutes.js
+++ routes/userMeRoutes.js
@@ -5,5 +5,11 @@ const router = express.Router();
 const userDBController = require("../db/controllers/userDBController");
 
 router.get("/me", (req, res, next) => {
-  
+  userDBController.findById(req.currentUserId)
+    .then(user => {
+      
+    })
+    .catch(error => {
+      next(createHttpError(404, JSON.stringify([errorMessages.AUTH_API_F_0008()])));
+    });
 });
