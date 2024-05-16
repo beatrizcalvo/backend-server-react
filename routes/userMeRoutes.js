660d277c2faef63c25fa4d2ee032677fba460ea7
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 22:25:43 +0200

    Update userMeRoutes.js

--- routes/userMeRoutes.js
+++ routes/userMeRoutes.js
@@ -2,7 +2,10 @@ const createHttpError = require('http-errors');
 const express = require("express");
 const router = express.Router();
 
+const errorMessages = require("../constants/errorConstants");
+
 const userDBController = require("../db/controllers/userDBController");
+const { userResponseDTO } = require("./dto/userDTO");
 
 router.get("/me", (req, res, next) => {
   userDBController.findById(req.currentUserId)
