b01af696ce41e9749d21269047b4328616d4ca16
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 14:15:06 +0200

    Update userDBController.js

--- db/controllers/userDBController.js
+++ db/controllers/userDBController.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose");
+const Users = require("../models/userModel");
 
+const NON_SELECTED_FIELDS = "-__v";
+
+const createUser = async function (firstName, lastName, email, password) {
+  let session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    // Save profile data
+    
+    // Commit the changes
+    await session.commitTransaction();
+  } catch (error) {
+    // Rollback any changes made in the database
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    session.endSession();
+  }
+};
+
+module.exports = { 
+  createUser
+};
