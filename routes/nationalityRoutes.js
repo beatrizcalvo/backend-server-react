e0923c77cfd2f000c53095c2f9771bbe53936d1e
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 11:26:19 +0200

    Update nationalityRoutes.js

--- routes/nationalityRoutes.js
+++ routes/nationalityRoutes.js
@@ -8,7 +8,22 @@ router.get("/", (req, res, next) => {
   nationalityDBController.findAllActive()
     .then(nationalities => {
       // Check if no exists nationalities active
-      res.status(200).send({});
+      if (nationalities.length === 0) {
+        console.log("GET /nationalities || Response Status: 204");
+        return res.status(204).send();
+      }
+
+      const responseBody = {
+        nationalities: nationalities.map(nationality => {
+          return({
+            nationalityId: nationality._id,
+            nationalityCode: nationality.code,
+            nationalityDescription: nationality.description
+          });
+        })
+      };
+      console.log("GET /nationalities || Response Status: 200 ## Response Body: " + JSON.stringify(responseBody));
+      res.status(200).send(responseBody);
     })
     .catch(error => {
       next(createHttpError(500, error));
