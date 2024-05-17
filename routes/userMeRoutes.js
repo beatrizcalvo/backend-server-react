dcc6b40aa10ff8fc76cacae1ec37430277ab8cbb
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-17 09:13:24 +0200

    Update userMeRoutes.js

--- routes/userMeRoutes.js
+++ routes/userMeRoutes.js
@@ -14,7 +14,7 @@ router.get("/me", (req, res, next) => {
       profileDBController.findByIdPopulated(user.profileId)
         .then(profile => {
           console.log(profile);
-          const responseBody = userResponseDTO(user);
+          const responseBody = userResponseDTO(user, profile);
           console.log("GET /users/me || Response Status: 200 ## Response Body: " + JSON.stringify(responseBody));
           res.status(200).send(responseBody);
         });
