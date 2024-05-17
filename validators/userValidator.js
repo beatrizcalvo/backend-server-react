ca77bd5b89e88824b2f297ef3f8f463187a94f59
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-17 17:21:28 +0200

    Create userValidator.js

---
+++ validators/userValidator.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+const updateSchema = Joi.object({    
+  active: Joi.boolean().optional(),
+  person: {
+    personName: {
+      firstName: Joi.string().min(3).optional(),
+      lastName: Joi.string().min(3).optional(),
+      secondLastName: Joi.string().allow("").optional()
+    },
+    gender: Joi.string().valid("Female", "Male").optional(),
+    birthDate: Joi.date().iso().optional(),
+    firstNationality: {
+      code: Joi.string().optional()
+    }
+  }
+});
+
+module.exports = { updateSchema }
