b13ce1e7491d358348478d7700591e44fd720058
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-20 16:31:13 +0200

    Update userMeRoutes.js

--- routes/userMeRoutes.js
+++ routes/userMeRoutes.js
@@ -41,6 +41,7 @@ router.patch("/me", validateRequest(updateSchema), async (req, res, next) => {
     const newUserFields = {
       ...(req.body.active && { active: req.body.active })
     };
+    console.log(req.body);
     console.log(newUserFields);
     // Check if there are fields to update
     if (Object.keys(newUserFields).length === 0) {
