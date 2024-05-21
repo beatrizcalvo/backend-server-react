5bc9b371ef2f30cf9ea035c06df46ab4f0240cba
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-21 17:28:26 +0200

    Update profileDBController.js

--- db/controllers/profileDBController.js
+++ db/controllers/profileDBController.js
@@ -5,12 +5,10 @@ const Role = require("../models/roleModel");
 const NON_SELECTED_FIELDS = "-__v";
 
 const findByIdPopulated = function (id) {
-  const profileFind = Profile.findById(id, NON_SELECTED_FIELDS).populate([
+  return Profile.findById(id, NON_SELECTED_FIELDS).populate([
     { path: "firstNationality", select: "code description", model: Nationality },
     { path: "role", select: "code description", model: Role }
-  ]).exec();
-  profileFind.formatDate("birthDate");
-  return profileFind;
+  ]);
 };
 
 module.exports = { findByIdPopulated }
