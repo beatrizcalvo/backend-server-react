77a2a448a0ae546158ce687e19214853c636056c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-21 13:09:22 +0200

    Update userDBController.js

--- db/controllers/userDBController.js
+++ db/controllers/userDBController.js
@@ -13,7 +13,7 @@ const NON_SELECTED_FIELDS = "-__v";
 const verifyFieldsModif = function (objFieldsModif, objDB) {
   Object.entries(objFieldsModif).forEach(([key, value]) => {
     console.log(key + " newValue: " + objFieldsModif[key] + " oldValue: ");
-    return value === objDB[key] && delete objFieldsModif[key];
+    value === objDB[key] && delete objFieldsModif[key];
   });
 };
 
