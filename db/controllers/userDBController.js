94556a72e33734ac39aaa1d2a53c9e77b1d02e0f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-21 12:30:13 +0200

    Update userDBController.js

--- db/controllers/userDBController.js
+++ db/controllers/userDBController.js
@@ -62,7 +62,7 @@ const updateUser = async function (id, updateFields) {
     }
 
     // Find profile to update, verify modifications and update if needed
-    const profileToUpdate = await profileDBController.findByIdPopulated(userToUpdate.profileId).lean().exec();
+    const profileToUpdate = await profileDBController.findByIdPopulated(userToUpdate.profileId);
     if (updateFieldsProfile !== null) {
       verifyFieldsModif(updateFieldsProfile, profileToUpdate);
       if (Object.keys(updateFieldsProfile).length !== 0) {
