b9ec2675ecc2ea51ab7c5ba391b73264d3b7ce3e
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-21 17:51:42 +0200

    Update userDBController.js

--- db/controllers/userDBController.js
+++ db/controllers/userDBController.js
@@ -12,6 +12,7 @@ const NON_SELECTED_FIELDS = "-__v";
 // Remove in objFieldsModif fields with equal value in objDB
 const verifyFieldsModif = function (objFieldsModif, objDB) {
   Object.entries(objFieldsModif).forEach(([key, value]) => {
+    console.log(typeof objFieldsModif[key]);
     console.log(key + " newValue: " + objFieldsModif[key] + " oldValue: " + objDB[key]);
     value === objDB[key] && delete objFieldsModif[key];
   });
