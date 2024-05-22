5752e6939b4dfdbcb68040c2591046fb56c8e4fb
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-22 09:40:06 +0200

    Update userDBController.js

--- db/controllers/userDBController.js
+++ db/controllers/userDBController.js
@@ -14,7 +14,7 @@ const verifyFieldsModif = function (objFieldsModif, objDB) {
   const dateFields = ["birthDate"];
   
   Object.entries(objFieldsModif).forEach(([key, value]) => {
-    console.log(Object.prototype.toString.call(objDB[key]));
+    console.log(Object.prototype.toString.call(objFieldsModif[key]));
     const newValue = value;
     console.log(newValue);
     const oldValue = ((Object.prototype.toString.call(objDB[key]) === "[object Date]") && new Date(objDB[key]).toISOString().slice(0, 10)) 
