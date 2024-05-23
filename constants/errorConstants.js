f9469f67bb56d460ade2f6359fe82ed9feca88ca
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-23 11:31:08 +0200

    Update errorConstants.js

--- constants/errorConstants.js
+++ constants/errorConstants.js
@@ -20,7 +20,7 @@ const AUTH_API_F_0003 = function (field, min) {
   return {
     code: "AUTH_API-F-0003",
     level: "error",
-    message: "Invalid imput size",
+    message: "Invalid input size",
     description: "Input field " + field + " length must be at least " + min + " characters long"
   };
 };
@@ -115,6 +115,15 @@ const AUTH_API_F_0013 = function () {
   };
 };
 
+const AUTH_API_F_0014 = function (field, max) {
+  return {
+      code: "AUTH_API-F-0014",
+      level: "error",
+      message: "Invalid array size",
+      description: "Input field " + field + " must contain less than or equal to " + max + " items"
+  };
+};
+
 const AUTH_API_T_0001 = function (descriptionError) {
   return {
     code: "AUTH_API-T-0001",
@@ -136,6 +145,6 @@ const AUTH_API_T_0002 = function (descriptionError) {
 module.exports = { 
   AUTH_API_F_0001, AUTH_API_F_0002, AUTH_API_F_0003, AUTH_API_F_0004, AUTH_API_F_0005, AUTH_API_F_0006, 
   AUTH_API_F_0007, AUTH_API_F_0008, AUTH_API_F_0009, AUTH_API_F_0010, AUTH_API_F_0011, AUTH_API_F_0012, 
-  AUTH_API_F_0013,
+  AUTH_API_F_0013, AUTH_API_F_0014,
   AUTH_API_T_0001, AUTH_API_T_0002
 };
