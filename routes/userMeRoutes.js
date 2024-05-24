f9868dd4cbbe66e3f568e290f0f2af00ca3deec6
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-24 14:51:10 +0200

    Update userMeRoutes.js

--- routes/userMeRoutes.js
+++ routes/userMeRoutes.js
@@ -57,7 +57,8 @@ router.patch("/me", validateRequest(updateSchema), async (req, res, next) => {
     };
     const newPostalAddressFields = {
       ...(req.body.contactPoint?.postalAddress?.addressLines?.length > 0 && { addressLine1: req.body.contactPoint.postalAddress.addressLines[0] }),
-      ...(req.body.contactPoint?.postalAddress?.addressLines?.length > 1 && { addressLine2: req.body.contactPoint.postalAddress.addressLines[1] })
+      ...(req.body.contactPoint?.postalAddress?.addressLines?.length > 1 && { addressLine2: req.body.contactPoint.postalAddress.addressLines[1] }),
+      ...(req.body.contactPoint?.postalAddress?.city && { city: req.body.contactPoint.postalAddress.city })
     };
     const newUserFields = {
       ...(req.body.active && { active: req.body.active }),
